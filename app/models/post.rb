da6ae12d93ca787098397502054bbe53d95455fc
Anna Dybas <a.m.dybas@gmail.com>
2020-01-16 14:50:29 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,31 @@
+require 'pry'
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate :is_clickbait?
+
+    # def click_bait_title
+    #     click_bait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+    #     if !click_bait.each {|bait| self.title.include?(bait)}
+    #         errors.add(:title, "Must include click bait buzzwords")
+    #     end
+    # end
+
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def is_clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
+
+
