117a203c7cde827b75e81639147d4f94de2a0d14
github username <github email address>
2020-01-17 11:40:08 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,19 @@ validates :title, presence: true
 validates :content, presence: true, length: {minimum: 250}
 validates :summary, presence: true, length: {maximum: 250}
 validates :category, presence: true, inclusion: {in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
+validate :is_clickbait?
+
+CLICKBAIT_PATTERNS = [
+  /Won't Believe/i,
+  /Secret/i,
+  /Top [0-9]*/i,
+  /Guess/i
+]
+
+def is_clickbait?
+  if CLICKBAIT_PATTERNS.none? { |pattern| pattern.match title }
+    errors.add(:title, "must be clickbait")
+  end
+end
 
 end
