11b0b2a1b1e6edfb577f6d2e9bf2be5d5e67a6f8
github username <github email address>
2020-01-17 11:38:08 -0500

    pause

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+validates :title, presence: true
+validates :content, presence: true, length: {minimum: 250}
+validates :summary, presence: true, length: {maximum: 250}
+validates :category, presence: true, inclusion: {in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category"}
+
 end
